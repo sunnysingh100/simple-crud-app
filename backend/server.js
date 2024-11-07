f5b9cb0dd1f4bd27feb8049a79f820494f0332c1
Sunny Singh <sunnysingh706050@gmail.com>
2024-11-07 18:28:36 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,24 @@ import express from "express";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
+import path from "path";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); //allows us to accept json data in the request body
 app.use("/api/products", productRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+}
 
 app.listen(PORT, () => {
   connectDB();
